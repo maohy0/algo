68b517bb13cea160e62c5b21196cf1ce3d022bd9
maohy0 <m3747hongyu@qq.com>
2025-05-19 22:43:58 +0800

    Update 05 Stack and Queue

--- 05 栈和队列/5.2-用队列实现栈.py
+++ 05 栈和队列/5.2-用队列实现栈.py
@@ -3,12 +3,11 @@ from collections import deque
 class myStack:
     def __init__(self):
         # deque是双向队列，可以用appendleft从左边添加元素，也可以用popleft从左边删除元素并返回
-        self.queue_in = deque()
-        self.queue_out = deque()
+        self.q = deque()
 
     # 将元素 x 压入栈顶
     def push(self, x: int) -> None:
-        self.queue_in.append(x)
+        self.q.append(x)
 
     # 移除并返回栈顶元素
     def pop(self) -> int:
@@ -16,18 +15,17 @@ class myStack:
             return None
 
         # 这个for循环，把queue_in除了最后一个元素，都加到了queue_out的右边，执行完之后queue_in只剩最后一个元素
-        for _ in range(len(self.queue_in) - 1):
-            self.queue_out.append(self.queue_in.popleft())
+        for _ in range(len(self.q) - 1):
+            self.q.append(self.q.popleft())
 
-        self.queue_in, self.queue_out = self.queue_out, self.queue_in   # 交换queue_in和out，只有in存数据
-        return self.queue_out.popleft()
+        return self.q.popleft()
 
     # 返回栈顶元素
     def top(self) -> int:
         if self.empty():
             return None
 
-        return self.queue_in[-1]
+        return self.q[-1]
 
     def empty(self) -> bool:
-        return len(self.queue_in) == 0   # 只有 queue_in 存了数据
+        return len(self.q) == 0   # 只有 queue_in 存了数据
